dbf46611b5298585e388e25778a4238a4d4cfdb5
vectorsize <vectorsize@gmail.com>
2016-06-05 12:50:05 +0200

    splits data into files

---
+++ viz/src/data/index.js
@@ -0,0 +1,26 @@
+import V from './v'
+import S from './s'
+import MP from './mp'
+import C from './c'
+import FP from './fp'
+import KD from './kd'
+import M from './m'
+import SD from './sd'
+
+const initialData = {
+  selected: 'C',
+  index: ['V', 'S', 'MP', 'C', 'FP', 'KD', 'M', 'SD'],
+  parties: {
+    V: 'Vänsterpartiet',
+    S: 'Socialdemokraterna',
+    MP: 'Miljöpartiet',
+    C: 'Centerpartiet',
+    FP: 'Liberalerna',
+    KD: 'Kristdemokraterna',
+    M: 'Moderaterna',
+    SD: 'Sverigedemokraterna'
+  },
+  data: { FP, S, M, MP, SD, C, KD, V },
+}
+
+export default initialData
